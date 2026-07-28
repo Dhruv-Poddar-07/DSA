807e5edb3947d6c8ede85293de31a415ede30b16
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-07-28 22:26:57 +0530

    managing time complexity

--- Division.cpp
+++ Division.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 void printDivisor(int n){
-    for(int i = 1 ; i<=n ; i++){
+    vector<int> divisors;
+    
+    for(int i = 1 ; i*i<=n ; i++){
         if(n%i==0){
-            cout<<i<<endl;
+            divisors.push_back(i);
+            if(i != n/i) divisors.push_back(n/i);
         }
     }
+    sort(divisors.begin(), divisors.end());
+    for(int d : divisors) cout << d << endl;
+
 }
 int main(){
     int n;
 
     cout<<"Enter Your Number: ";cin>>n;
     printDivisor(n);
-
+    
     return 0;
 }
