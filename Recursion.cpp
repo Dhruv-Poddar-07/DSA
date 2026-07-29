af0b75213ca56cee202e0534d77d85f3374048a5
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-07-29 22:38:37 +0530

    To print name n entered times

---
+++ Recursion.cpp
@@ -0,0 +1,19 @@
+// basic
+#include <iostream>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n) return; // if a user enters the number of time to be printed as 0
+    cout<<"Dhruv"<<endl;
+    f(i+1,n);
+}
+
+int main(){
+    int num;
+    cout<<"Enter Number of times: ";cin>>num;
+    f(1,num);
+    return 0;
+}
+
+
+
