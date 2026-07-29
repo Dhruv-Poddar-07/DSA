30420116f889dca8f37ee8f847fcc16ca6253667
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-07-29 22:55:32 +0530

    To print number linearly till the entered number

--- LinearlyPrint.cpp
+++ LinearlyPrint.cpp
@@ -14,3 +14,21 @@ int main(){
     print(1,num);
     return 0; 
 }
+
+
+
+//to print linear in reverse order
+// void print(int i , int n){
+//     if(i<1)return;
+//     cout<<i<<endl;
+//     i--;
+//     print(i,n);
+// }
+
+// int main(){
+//     int num;
+//     cout<<"Enter Number: ";cin>>num;
+//     print(num,num);
+//     return 0; 
+// }
+
