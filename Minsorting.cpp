473e6d4295410e373036d13706649f3ac3eaef77
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-08-05 23:13:58 +0530

    To learn the sort of the array by arranging the elements in desending order

---
+++ Minsorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[] , int n){
+    for(int i = 0; i<=n-1 ; i++){
+        int min = i;
+        for(int j  = 0 ; j<=n-1 ; j++){
+            if(arr[j]<arr[min]){ min = j;}
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main(){
+    int n;
+
+    cout<<"enter number of inputs: ";cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter array: ";
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    
+
+    return 0;
+}
