b570818eed9797c1a1906c8b217b0b5e3176cb25
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-08-05 23:24:32 +0530

    Corrected inner loop

--- Minsorting.cpp
+++ Minsorting.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void sort(int arr[] , int n){
     for(int i = 0; i<=n-1 ; i++){
         int min = i;
-        for(int j  = 0 ; j<=n-1 ; j++){
+        for(int j  = i ; j<=n-1 ; j++){
             if(arr[j]<arr[min]){ min = j;}
         }
         int temp = arr[min];
