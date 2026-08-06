1c6a2bd3406cf7a3def2a5421468a5fe2f821f6b
Dhruv-Poddar-07 <dhruvpoddar100@gmail.com>
2026-08-07 00:00:17 +0530

    Bubble sort full code

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -15,6 +15,18 @@ void bubble_sort(int arr[],int n){
 
 int main(){
     int n;
-    cout<<"Enter Number of elem"
+    cout<<"Enter Number of elements in array: ";cin>>n;
+    int arr[n];
+    cout<<"Enter elements: ";
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+
+    bubble_sort(arr,n);
+    
+    cout<<"Arranged Array:";
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
